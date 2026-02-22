f819764b35f54024eb12224c93466c56a1fdd67d
Mehrab <mmehrab.pk@gmail.com>
2026-02-22 18:32:41 +0330

    Send updated

--- src/components/api-checker/send/Send.jsx
+++ src/components/api-checker/send/Send.jsx
@@ -1,7 +1,73 @@
+// "use client";
+
+// import React, { useState } from "react";
+// import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+// import { Input } from "@/components/ui/input";
+// import {
+//   Select,
+//   SelectContent,
+//   SelectGroup,
+//   SelectItem,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select";
+// import { Button } from "@/components/ui/button";
+// import { IconSend2 } from "@tabler/icons-react";
+// import { Spinner } from "@/components/ui/spinner";
+// import { toast } from "sonner"
+
+// export default function Send({ onSend, loading }) {
+//   const [url, setUrl] = useState("");
+//   const [method, setMethod] = useState("");
+//   const handleSned = () => {
+//     if (!url.trim()) return;
+//     onSend({
+//       url,
+//       method,
+//     });
+//   };
+//   return (
+//     <div className="w-full border-b border-border px-8 py-4">
+//       <Field className="flex flex-row">
+//         <Input
+//           className="w-[50%]!"
+//           id="input-url"
+//           type="text"
+//           placeholder="https://jsonplaceholder.typicode.com/posts"
+//           onChange={(e) => setUrl(e.target.value)}
+//         />
+//         <Select onValueChange={setMethod}>
+//           <SelectTrigger className="w-[30%]!">
+//             <SelectValue placeholder="Method" />
+//           </SelectTrigger>
+//           <SelectContent>
+//             <SelectGroup>
+//               <SelectItem value="GET">Get</SelectItem>
+//               <SelectItem value="POST">Post</SelectItem>
+//               <SelectItem value="PUT">Put</SelectItem>
+//               <SelectItem value="DELETE">Delete</SelectItem>
+//             </SelectGroup>
+//           </SelectContent>
+//         </Select>
+//         {loading ? (
+//           <Button variant="outline" disabled className="w-[20%]!">
+//             <Spinner data-icon="inline-start" />
+//             Please wait
+//           </Button>
+//         ) : (
+//           <Button variant="outline" className="w-[20%]!" onClick={handleSned}>
+//             Send <IconSend2 />
+//           </Button>
+//         )}
+//       </Field>
+//     </div>
+//   );
+// }
+
 "use client";
 
 import React, { useState } from "react";
-import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Field } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import {
   Select,
@@ -14,18 +80,28 @@ import {
 import { Button } from "@/components/ui/button";
 import { IconSend2 } from "@tabler/icons-react";
 import { Spinner } from "@/components/ui/spinner";
-import { toast } from "sonner"
+import { toast } from "sonner"; // shadcn compatible toast
 
 export default function Send({ onSend, loading }) {
   const [url, setUrl] = useState("");
   const [method, setMethod] = useState("");
-  const handleSned = () => {
-    if (!url.trim()) return;
+
+  const handleSend = () => {
+    if (!url.trim()) {
+      toast.error("URL cannot be empty!");
+      return;
+    }
+    if (!method) {
+      toast.error("Please select a method!");
+      return;
+    }
+
     onSend({
       url,
       method,
     });
   };
+
   return (
     <div className="w-full border-b border-border px-8 py-4">
       <Field className="flex flex-row">
@@ -55,7 +131,7 @@ export default function Send({ onSend, loading }) {
             Please wait
           </Button>
         ) : (
-          <Button variant="outline" className="w-[20%]!" onClick={handleSned}>
+          <Button variant="outline" className="w-[20%]!" onClick={handleSend}>
             Send <IconSend2 />
           </Button>
         )}
