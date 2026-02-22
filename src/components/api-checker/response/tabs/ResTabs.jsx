7ae0ee4861e78908f305bc92dbb20ca7c1cd19a2
Mehrab <mmehrab.pk@gmail.com>
2026-02-22 22:09:15 +0330

    update

--- src/components/api-checker/response/tabs/ResTabs.jsx
+++ src/components/api-checker/response/tabs/ResTabs.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Card,
   CardContent,
@@ -6,33 +6,40 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import ApiDocumentation from './ApiDocumentation';
-import ResponseBody from './ResponseBody';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import ApiDocumentation from "./ApiDocumentation";
+import ResponseBody from "./ResponseBody";
+import useApiStore from "@/app/store/useApiStore";
 
 export default function ResTabs() {
+  const { url } = useApiStore();
   return (
-        <Tabs defaultValue="Documentation" className={'mt-6'}>
-          <TabsList>
-            <TabsTrigger value="Documentation">Documentation</TabsTrigger>
-            <TabsTrigger value="Headers">Headers</TabsTrigger>
-            <TabsTrigger value="Response">Response</TabsTrigger>
-          </TabsList>
-          <TabsContent value="Documentation">
-            <Card className={'p-4'}>
-              <ApiDocumentation />
-            </Card>
-          </TabsContent>
-          <TabsContent value="Headers">
-            <Card className={'p-4'}>
-              234
-            </Card>
-          </TabsContent>
-          <TabsContent value="Response">
-            <Card className={'p-4'}>
-              <ResponseBody />
-            </Card>
-          </TabsContent>
-        </Tabs>
-  )
+    <Tabs defaultValue="Documentation" className={"mt-6"}>
+      <TabsList>
+        <TabsTrigger value="Documentation">Documentation</TabsTrigger>
+        <TabsTrigger value="Headers">Headers</TabsTrigger>
+        <TabsTrigger value="Response">Response</TabsTrigger>
+      </TabsList>
+      <TabsContent value="Documentation">
+        <Card className={"h-70 p-4"}>
+          <ApiDocumentation />
+        </Card>
+      </TabsContent>
+      <TabsContent value="Headers">
+        <Card className={"h-70 p-4"}>234</Card>
+      </TabsContent>
+      <TabsContent value="Response">
+        <Card className={"h-70 p-4"}>
+          {url ? (
+            <ResponseBody />
+          ) : (
+            <p className="text-[14px] capitalize">
+              After submitting the request, the documentation is automatically
+              created.
+            </p>
+          )}
+        </Card>
+      </TabsContent>
+    </Tabs>
+  );
 }
