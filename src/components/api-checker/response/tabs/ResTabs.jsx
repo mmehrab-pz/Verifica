ce469b482af63b1d35f2441703724133b95bf9f7
Mehrab <mmehrab.pk@gmail.com>
2026-02-25 00:48:23 +0330

    copy button created

--- src/components/api-checker/response/tabs/ResTabs.jsx
+++ src/components/api-checker/response/tabs/ResTabs.jsx
@@ -6,40 +6,60 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ApiDocumentation from "./ApiDocumentation";
 import ResponseBody from "./ResponseBody";
 import useApiStore from "@/app/store/useApiStore";
+import { IconCopy } from "@tabler/icons-react";
+import { toast } from "sonner"
 
 export default function ResTabs() {
-  const { url } = useApiStore();
+  const { url , data } = useApiStore();
+  const copyJson = ()=>{
+    navigator.clipboard.writeText(JSON.stringify(data, null, 2))
+    toast.success("JSON copied successfully", { position: "top-center" })
+  }
   return (
-    <Tabs defaultValue="Documentation" className={"mt-6"}>
-      <TabsList>
-        <TabsTrigger value="Documentation">Documentation</TabsTrigger>
-        <TabsTrigger value="Headers">Headers</TabsTrigger>
-        <TabsTrigger value="Response">Response</TabsTrigger>
-      </TabsList>
-      <TabsContent value="Documentation">
-        <Card className={"min-h-70 p-4"}>
-          <ApiDocumentation />
-        </Card>
-      </TabsContent>
-      <TabsContent value="Headers">
-        <Card className={"min-h-70 p-4"}>234</Card>
-      </TabsContent>
-      <TabsContent value="Response">
-        <Card className={"min-h-70 p-4"}>
+    <>
+      <Tabs defaultValue="Documentation" className={"mt-6"}>
+        <div className="flex items-center gap-2.5">
+          <TabsList>
+            <TabsTrigger value="Documentation">Documentation</TabsTrigger>
+            <TabsTrigger value="Headers">Headers</TabsTrigger>
+            <TabsTrigger value="Response">Response</TabsTrigger>
+          </TabsList>
           {url ? (
-            <ResponseBody />
+            <Button variant="outline" size="icon" onClick={copyJson} >
+            <IconCopy />
+          </Button>
           ) : (
-            <p className="text-[14px] capitalize">
-              After submitting the request, the documentation is automatically
-              created.
-            </p>
+            <Button disabled variant="outline" size="icon" onClick={copyJson} >
+            <IconCopy />
+          </Button>
           )}
-        </Card>
-      </TabsContent>
-    </Tabs>
+        </div>
+        <TabsContent value="Documentation">
+          <Card className={"min-h-70 p-4"}>
+            <ApiDocumentation />
+          </Card>
+        </TabsContent>
+        <TabsContent value="Headers">
+          <Card className={"min-h-70 p-4"}>234</Card>
+        </TabsContent>
+        <TabsContent value="Response">
+          <Card className={"min-h-70 p-4"}>
+            {url ? (
+              <ResponseBody />
+            ) : (
+              <p className="text-[14px] capitalize">
+                After submitting the request, the documentation is automatically
+                created.
+              </p>
+            )}
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </>
   );
 }
